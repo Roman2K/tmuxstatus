ec685c93a1d85e2c9d1b4cc8d6ade310e5d8d2c3
Roman Le Négrate <roman.lenegrate@gmail.com>
2017-02-27 19:02:02 +0100

    exclude PID of tmuxstatus

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -16,31 +16,35 @@ type topLine struct {
 
 func Top(n int) ([]topLine, error) {
 	uid := fmt.Sprintf("%d", os.Getuid())
-	cmd := exec.Command("ps", "-U", uid, "-e", "-o", "%cpu,comm")
-
+	cmd := exec.Command("ps", "-U", uid, "-e", "-o", "pid,%cpu,comm")
 	out, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
-
 	topLines := []topLine{}
-
+	curPid := os.Getpid()
 	split := bufio.NewScanner(bytes.NewReader(out))
 	split.Scan() // skip headers
 	for split.Scan() {
-		var topl topLine
-		_, err := fmt.Sscanf(split.Text(), "%f %s", &topl.Pct, &topl.Command)
+		var (
+			topl topLine
+			pid  int
+		)
+		_, err := fmt.Sscanf(split.Text(), "%d %f %s",
+			&pid, &topl.Pct, &topl.Command,
+		)
 		if err != nil {
+			continue
+		}
+		if pid == curPid {
 			continue
 		}
 		topLines = append(topLines, topl)
 	}
-
 	if l := len(topLines); n > l {
 		n = l
 	}
 	sort.Sort(byPct(topLines))
-
 	return topLines[:n], nil
 }
 
